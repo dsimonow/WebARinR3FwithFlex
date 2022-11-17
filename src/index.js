ee519e93b85c76b736c5c7aa2eb9d1ff144f627e
simonow@adesso <Dimitri.Simonow@adesso.de>
2022-11-17 14:48:54 +0100

    yarn update zu 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
+import { createRoot } from 'react-dom/client'
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+createRoot(document.getElementById('root')).render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
